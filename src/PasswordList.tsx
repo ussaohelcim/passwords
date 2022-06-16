91df4344bdd2cf6d2f3d4919f9e859a1e9d386d8
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-06-16 10:54:05 -0300

    added random password generator

--- src/PasswordList.tsx
+++ src/PasswordList.tsx
@@ -86,10 +86,26 @@ export class NewAccount extends React.Component{
 	}
 	
 	addNewAccount(acc:IAccount){
-		//@ts-ignore
-		// window.PasswordList.alertMessage();
 		window.dispatchEvent(new Event(refreshEvent))
 		db.add(acc)
+	}
+
+	generateRandomPassword(btn:any){
+		const passwordForm = btn.target.parentNode as HTMLFormElement
+		const inputBox = passwordForm!.querySelector("#passwordInputBox") as HTMLInputElement
+
+		let newPass = ""
+		let passLength = 20
+
+		for (let index = 0; index < passLength; index++) {
+			//32 = " "
+			//126 = "~"
+			let dec = Math.floor(Math.random() * (126-32) ) + 32
+
+			newPass += String.fromCharCode(dec)
+		}
+		
+		inputBox.value = newPass
 	}
 
 	render(): React.ReactNode {
@@ -98,8 +114,9 @@ export class NewAccount extends React.Component{
 			<form onSubmit={this.handleForm.bind(this)}>
 				<input type="text" name="service" placeholder='service name' id="" />
 				<input type="text" name="username" placeholder='username' id="" />
-				<input type="text" name="password" placeholder='password' id=""/>
-				<input type="submit" value="add account" />
+				<input type="text" name="password" placeholder='password' id="passwordInputBox" />
+				<input type="button" value="Generate random pass" onClick={this.generateRandomPassword.bind(this)}/>
+				<input type="submit" value="Add account" />
 			</form>
 		</div>
 	}
